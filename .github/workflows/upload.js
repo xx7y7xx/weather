a9d44f4747ecdb60d9c46c7417c36bd879b60aa9
Deleted user <5204106+xx7y7xx@users.noreply.github.com>
2023-07-17 16:29:14 +0000

    nodejs upload

---
+++ .github/workflows/upload.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const {google} = require('googleapis');
+const keyFile = 'credentials.json';
+
+const drive = google.drive({
+  version: 'v3',
+  auth: new google.auth.GoogleAuth({
+    keyFile: keyFile,
+    scopes: ['https://www.googleapis.com/auth/drive'],
+  }),
+});
+
+async function uploadFile(filePath) {
+  const fileMetadata = {
+    'name': filePath.split('/').pop(),
+    'parents': [process.env.FOLDERID],
+  };
+  const media = {
+    mimeType: 'image/png',
+    body: fs.createReadStream(filePath),
+  };
+  try {
+    const response = await drive.files.create({
+      requestBody: fileMetadata,
+      media,
+      fields: 'id',
+    });
+    console.log(`File ID: ${response.data.id}, File: ${filePath} - Uploaded`);
+  } catch (err) {
+    console.error(`Error uploading ${filePath}:`, err);
+  }
+}
+
+async function uploadFiles() {
+  const files = fs.readdirSync('.').filter((file) => file.startsWith('radar_frame_timestamp_'));
+  for (const file of files) {
+    await uploadFile(file);
+  }
+}
+
+uploadFiles();
---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+
+#
+# install
+#
+
+apt-get update
+apt-get install -y gifsicle imagemagick tesseract-ocr tesseract-ocr-eng
+ls -l
+
+#
+# download
+#
+
+curl -s -o new_radar.gif "https://weather.bangkok.go.th/Images/Radar/radar.gif"
+ls -l
+
+#
+# split
+#
+
+# Split GIF into static images
+convert -coalesce new_radar.gif new_radar_frame_%d.png
+ls -l
+
+# Extract timestamp and rename static images
+for file in new_radar_frame_*.png; do
+  # Crop the image using the specified coordinates and dimensions
+  convert "${file}" -crop 120x19+838+728 "cropped_${file}"
+
+  timestamp=$(tesseract "cropped_${file}" stdout -c tessedit_char_whitelist='0123456789-: ' --psm 6 | tr -d '[:space:]')
+  if [ ! -z "$timestamp" ]; then
+    # timestamp="2023-07-1421:35:00"
+    # formatted_timestamp="2023-07-14 21:35:00"
+    formatted_timestamp=$(echo "${timestamp}" | sed 's/\([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\)/\1 /')
+    mv "${file}" "radar_frame_timestamp_${formatted_timestamp}.png"
+  fi
+
+  rm "cropped_${file}"
+done
+ls -l
