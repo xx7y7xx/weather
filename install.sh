354ec07d636e37ea7c6d9ec92175762677016389
Deleted user <5204106+xx7y7xx@users.noreply.github.com>
2023-07-18 14:15:34 +0000

    try

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 uname -a
 
+# install tesseract v5 (without this, ubuntu only has v4)
+sudo add-apt-repository -y ppa:alex-p/tesseract-ocr5
+
 sudo apt-get update
 sudo apt-get install -y gifsicle imagemagick tesseract-ocr tesseract-ocr-eng
-
-## tesseract v4
-#sudo apt-get install -y tesseract-ocr tesseract-ocr-eng
-# tesseract v5
-sudo add-apt-repository -y ppa:alex-p/tesseract-ocr5
-sudo apt install -y tesseract-ocr tesseract-ocr-eng
 
 # version check
 convert --version
 tesseract --version
+
+npm i
+
+cat /home/runner/work/weather/weather/node_modules/googleapis/node_modules/gtoken/build/src/index.js
