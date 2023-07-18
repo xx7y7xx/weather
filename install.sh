8b70f7bde957ec4d8940af424cbf430c32d0ad7a
Deleted user <5204106+xx7y7xx@users.noreply.github.com>
2023-07-18 14:08:59 +0000

    improve

--- install.sh
+++ install.sh
@@ -4,3 +4,13 @@ uname -a
 
 sudo apt-get update
 sudo apt-get install -y gifsicle imagemagick tesseract-ocr tesseract-ocr-eng
+
+## tesseract v4
+#sudo apt-get install -y tesseract-ocr tesseract-ocr-eng
+# tesseract v5
+sudo add-apt-repository -y ppa:alex-p/tesseract-ocr5
+sudo apt install -y tesseract-ocr tesseract-ocr-eng
+
+# version check
+convert --version
+tesseract --version
